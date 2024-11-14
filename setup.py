f5907fc474122625870e241d5620139392305257
Chris Gravel <cpagravel@gmail.com>
2024-11-13 21:04:32 -0800

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import setuptools
 setuptools.setup(
     name = "gst",
     packages = {".": "gst"},
-    version = "0.1.0",
+    version = "0.1.1",
     license="MIT",
     description = "Git status tool",
     author = "Chris Gravel",
     author_email = "cpagravel@gmail.com",
     url = "https://github.com/cpagravel/gst-python",
-    download_url = "https://github.com/cpagravel/gst-python/archive/0.1.0.tar.gz",    # I explain this later on
+    download_url = "https://github.com/cpagravel/gst-python/archive/0.1.1.tar.gz",
     keywords = ["git", "git status", "git workflow"],
     classifiers=[
         "Development Status :: 4 - Beta",
